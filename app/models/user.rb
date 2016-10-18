3e93dfbbfb6cffd877fd857801181e247f71cbf5
Vince Francesi <vincefrancesi@gmail.com>
2016-10-17 21:12:06 -0700

    Update to deliver now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   def self.send_emails
     User.all.each do |user|
-      PuppyMailer.todays_puppies(user).deliver
+      PuppyMailer.todays_puppies(user).deliver_now
     end
   end
 end
