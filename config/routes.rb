7f8a278b7be85b558cbe52e051d15018ae6a7deb
Mira Applegate <mirabaiapplegate@icloud.com>
2016-11-10 16:07:12 -0800

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get    '/signup',  to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :puppies, only: [:index]
+  resources :users
 
   root 'static#welcome'
 end
